ad32bba15514db11b9486a5acb9e59c895fe0ec9
Connor Gallopo <connor.gallopo@me.com>
2025-11-30 20:07:05 -0500

    configure eas build profiles and expo updates

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,21 +3,17 @@ const path = require('path');
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
-// This can be replaced with `auto` to find the monorepo root automatically
 const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
-// 1. Watch all files within the monorepo
-config.watchFolders = [monorepoRoot];
+// 1. Watch all files within the monorepo (include default watchFolders)
+config.watchFolders = [...(config.watchFolders || []), monorepoRoot];
 
 // 2. Let Metro know where to resolve packages from
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ];
-
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
